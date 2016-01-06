b29f7c83fb926000a7615c15c12a0d6a1ab659e8
gerrus <keine>
2016-01-06 15:40:12 +0100

    Von arbeitspc aus,umbau eigene klasse

---
+++ CrypterBibi/Crypter.cs
@@ -0,0 +1,110 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Security.Cryptography;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CrypterBibi
+{
+    public class Crypter
+    {
+        private byte[] _key;
+        private byte[] _iv;
+
+        void KeyundIVGenerator(string password, string salt)
+        {
+            byte[] _salt = Convert.FromBase64String(salt);
+            Rfc2898DeriveBytes pdb = new Rfc2898DeriveBytes(password, _salt);
+            _key = pdb.GetBytes(32);
+            _iv = pdb.GetBytes(16);
+        }
+
+        public void EncryptFile(string password, string inputFile, string outputFile)
+        {
+            try
+            {
+                /*--------------Passwort in Bytes umwandeln-----------*/
+                string verschlüsselterDateiName = DateiNameEncrypter.FileNameEncoder(inputFile);
+
+                KeyundIVGenerator(password, verschlüsselterDateiName);
+
+
+                //------ Ausgangsdatei erzeugen
+
+                // FileStream fsCrypt = new FileStream(cryptFile, FileMode.Create);
+
+
+                //------------ Den Crypter erzeugen
+                var crypter = MakeCryptor();
+
+
+                using ( FileStream fsCrypt = new FileStream(NeuerPfad(inputFile, verschlüsselterDateiName), FileMode.Create)   )
+                {
+                    using (CryptoStream cs = new CryptoStream(fsCrypt, crypter, CryptoStreamMode.Write))
+                    {
+                        using (FileStream fsIn = new FileStream(inputFile, FileMode.Open))
+                        {
+                            int data;
+                            while ((data = fsIn.ReadByte()) != -1)
+                                cs.WriteByte((byte) data);
+                        }
+                    }
+                }
+            }
+            catch (Exception e)
+            {
+                // MessageBox.Show($"Encryption failed! \n {e.Message}", "Error");
+            }
+        }
+        public void DecryptFile(string inputFile, string outputFile)
+        {
+
+            {
+               
+
+                byte[] _salt = Convert.FromBase64String("saltsaltsalt");
+                Rfc2898DeriveBytes pdb = new Rfc2898DeriveBytes(password, _salt);
+                var Key = pdb.GetBytes(32);
+                var IV = pdb.GetBytes(16);
+
+                string fileName = Path.GetDirectoryName(inputFile) + "\\" + base64Decode(Path.GetFileName(inputFile));
+                FileStream fsCrypt = new FileStream(inputFile, FileMode.Open);
+
+                RijndaelManaged RMCrypto = new RijndaelManaged();
+                RMCrypto.IV = IV;
+                RMCrypto.Key = Key;
+                CryptoStream cs = new CryptoStream(fsCrypt, RMCrypto.CreateDecryptor(), CryptoStreamMode.Read);
+
+                FileStream fsOut = new FileStream(fileName, FileMode.Create);
+
+                int data;
+                while ((data = cs.ReadByte()) != -1)
+                    fsOut.WriteByte((byte)data);
+
+                fsOut.Close();
+                cs.Close();
+                fsCrypt.Close();
+
+            }
+        }
+
+        private string NeuerPfad(string inputFile, string verschlüsselterDateiName)
+        {
+            return Path.GetDirectoryName(inputFile) + "\\" + verschlüsselterDateiName;
+        }
+
+        private ICryptoTransform MakeCryptor()
+        {
+            var RMCrypto = new RijndaelManaged
+            {
+                IV = _iv,
+                Key = _key
+            };
+
+            var crypter = RMCrypto.CreateEncryptor(RMCrypto.Key, RMCrypto.IV);
+            return crypter;
+        }
+    }
+}
---
+++ CrypterBibi/DateiNameEncrypter.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Text;
+
+namespace CrypterBibi
+{
+    public  static class DateiNameEncrypter
+    {
+        
+        /// <summary>
+        ///  Entkodiert einen String , in dem Fall den Dateinamen
+        /// </summary>
+        /// <param name="fileName">Der zu verschlüsselnde Dateiname</param>
+        /// <returns>Der verschlüsselte string</returns>
+        public static string FileNameEncoder(string fileName)
+        {
+            try
+            {
+                byte[] encData_byte = new byte[fileName.Length];
+                encData_byte = Encoding.UTF8.GetBytes(fileName);
+                string encodedData = Convert.ToBase64String(encData_byte);
+                return encodedData;
+            }
+            catch (Exception e)
+            {
+                throw new Exception("Error in FileNameEncoder" + e.Message);
+            }
+        }
+
+        /// <summary>
+        /// Dekodiert einen String , in dem Fall den Dateinamen
+        /// </summary>
+        /// <param name="fileName">Der zu entschlüsselnde String</param>
+        /// <returns>Der Dateiname</returns>
+        public static string FileNameDecoder(string fileName)
+        {
+            try
+            {
+                UTF8Encoding encoder = new UTF8Encoding();
+                Decoder utf8Decode = encoder.GetDecoder();
+
+                byte[] todecode_byte = Convert.FromBase64String(fileName);
+                int charCount = utf8Decode.GetCharCount(todecode_byte, 0, todecode_byte.Length);
+                char[] decoded_char = new char[charCount];
+                utf8Decode.GetChars(todecode_byte, 0, todecode_byte.Length, decoded_char, 0);
+                string result = new String(decoded_char);
+                return result;
+            }
+            catch (Exception e)
+            {
+                throw new Exception("Error in FileNameDecoder" + e.Message);
+            }
+        }
+       
+    }
+}
